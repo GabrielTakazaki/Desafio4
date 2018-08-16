65fabf6a07bde1ed850379fd46b060e69145b77c
GabrielTakazaki <gabriel_takazaki@hotmail.com>
2018-08-16 17:29:24 -0300

    Listando clientes cadastrados

--- ProjetoForlogic/js/cliente.js
+++ ProjetoForlogic/js/cliente.js
@@ -2,6 +2,7 @@ var nome = document.getElementById('nomeInput');
 var contato = document.getElementById('contatoInput');
 var data = document.getElementById('dataInput');
 var add = document.getElementById('cadastroButton');
+var lista = document.getElementById('lista');
 
 add.addEventListener('click', function () {
     create(nome.value, contato.value, data.value);
@@ -15,3 +16,12 @@ function create(nome, contato, data) {
     }
     return firebase.database().ref().child('clientes').push(dbase);
 }
+
+firebase.database().ref('clientes').on('value', function (snapshot) {
+    lista.innerHTML = '';
+    snapshot.forEach(function (item) {
+        var li = document.createElement('li');
+        li.appendChild(document.createTextNode(item.val().nome + ': ' + item.val().contato + ': ' + item.val().data));
+        lista.appendChild(li);
+    })
+})
