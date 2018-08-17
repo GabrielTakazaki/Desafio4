9eec31240b14aad261d505f6736e035c2238d32c
GabrielTakazaki <gabriel_takazaki@hotmail.com>
2018-08-17 16:00:21 -0300

    Alteracoes nas listagem e cadastro

--- ProjetoForlogic/js/avaliacao.js
+++ ProjetoForlogic/js/avaliacao.js
@@ -1,2 +1,10 @@
-var UsersRef = firebase.database().ref('clientes')
-var HoppersRef = UsersRef.child();
+
+var nomes = document.getElementById('nomes');
+
+firebase.database().ref('clientes').on('value', function (snapshot) {
+    snapshot.forEach(function (item) {
+        var clienteTable = document.createElement('label');
+        clienteTable.appendChild(document.createTextNode(item.val().nome));
+        nomes.appendChild(clienteTable);
+    })
+});
--- ProjetoForlogic/js/lista.js
+++ ProjetoForlogic/js/lista.js
@@ -6,15 +6,18 @@ var contato = document.getElementById('contato');
 var data = document.getElementById('data');
 var categoria = document.getElementById('categoria');
 var nenhumaAvaliacao = document.getElementById('nenhumaAvaliacao');
+var dbFirebase = firebase.database().ref('clientes');
 
-firebase.database().ref('clientes').on('value', function (snapshot) {
-    cliente.innerHTML = '';
+
+dbFirebase.orderByKey().on('value', function (snapshot) {
     snapshot.forEach(function (item) {
         var categoriaTable = document.createElement('table');
         categoriaTable.appendChild(document.createTextNode(item.val().categoria));
         if (categoriaTable.firstChild.nodeValue === "Nenhum: Sem avaliacao") {
+
             var clienteTable = document.createElement('table');
-            clienteTable.appendChild(document.createTextNode(item.val().nome));
+            clienteTable.appendChild(document.createTextNode(item.key));
+
             var contatoTable = document.createElement('table');
             contatoTable.appendChild(document.createTextNode(item.val().contato));
             var dataTable = document.createElement('table');
